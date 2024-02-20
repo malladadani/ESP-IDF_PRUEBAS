dbd8f5da78e8e39b8a6d43b546eb845e8d6d32cb
malladadani <138223991+malladadani@users.noreply.github.com>
2024-02-20 12:11:28 +0100

    Update Programa_1.c

--- Programa_1.c
+++ Programa_1.c
@@ -1,1 +1,31 @@
+/* 
 
+*/
+
+#include <stdio.h>
+
+#include "driver/gpio.h"
+
+
+#define Interruptor GPIO_NUM_5
+#define Led GPIO_NUM_23
+
+void conf_intr()
+{
+  gpio_num_t pin = Interruptor;
+  gpio_int_type_t tipo_interrupcion = GPIO_INTR_POSEDGE;
+
+  esp_err_t resultado = gpio_set_intr_type(pin,tipo_interrupcion);
+
+  if(resultado != ESP_OK)
+  {
+    printf("Erros en la interrupcion");
+  }
+
+  else
+  {
+    printf("OKAY");
+  }
+
+  
+}
